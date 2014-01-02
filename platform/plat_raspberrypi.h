700b9805e970c7c6b9912e296ced1b91b6806149
Richard Osterloh <richard.osterloh@gmail.com>
2014-01-02 15:29:02 +0000

    Configured for RPi with EVE board

--- platform/plat_raspberrypi.h
+++ platform/plat_raspberrypi.h
@@ -1,18 +1,18 @@
 /* rfm12b-linux: linux kernel driver for the rfm12(b) RF module by HopeRF
 *  Copyright (C) 2013 Georg Kaindl
-*  
+*
 *  This file is part of rfm12b-linux.
-*  
+*
 *  rfm12b-linux is free software: you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
 *  the Free Software Foundation, either version 2 of the License, or
 *  (at your option) any later version.
-*  
+*
 *  rfm12b-linux is distributed in the hope that it will be useful,
 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *  GNU General Public License for more details.
-*  
+*
 *  You should have received a copy of the GNU General Public License
 *  along with rfm12b-linux.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -25,7 +25,7 @@
 
 /*
    This file handles pinmuxing on Raspberry Pi.
-   
+
    If you want to change the default hardware settings for RPi, this
    is the place to look.
 */
@@ -45,7 +45,7 @@
 *  P1/17                 VDD (+3.3V)
 *  P1/19                 SDI
 *  P1/21                 SDO
-*  P1/22                 nIRQ
+*  P1/15                 nIRQ
 *  P1/23                 SCK
 *  P1/25                 GND
 *  P1/26                 nSEL
@@ -55,10 +55,11 @@
 *
 *  modprobe spi-bcm2708
 */
+#define RFM12_IRQ_GPIO 22
 
 struct spi_rfm12_board_config board_configs[NUM_RFM12_BOARDS] = {
    {
-      .irq_pin      = 25,   // gpio 25
+      .irq_pin      = RFM12_IRQ_GPIO,
       .spi_bus      = 0,   // spi port on P1 header
       .spi_cs      = 1   // CS 1
    }
@@ -73,23 +74,23 @@ static int
 spi_rfm12_init_pinmux_settings(void)
 {
 // taken from https://github.com/bootc/linux/blob/rpi-i2cspi/drivers/spi/spi-bcm2708.c
-   
+
 #define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
 #define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
 
    int pin;
    u32* gpio = ioremap(0x20200000, SZ_16K);
-   
+
    // SPI0 is on gpio 7..11
    for (pin = 7; pin <= 11; pin++) {
       INP_GPIO(pin);
       SET_GPIO_ALT(pin, 0);
    }
-   
+
    // irq pin
-   INP_GPIO(25);
-   SET_GPIO_ALT(25, 0);
-   
+   INP_GPIO(RFM12_IRQ_GPIO);
+   SET_GPIO_ALT(RFM12_IRQ_GPIO, 0);
+
    iounmap(gpio);
 
 #undef INP_GPIO
--- rfm12b_config.h
+++ rfm12b_config.h
@@ -1,18 +1,18 @@
 /* rfm12b-linux: linux kernel driver for the rfm12(b) RF module by HopeRF
 *  Copyright (C) 2013 Georg Kaindl
-*  
+*
 *  This file is part of rfm12b-linux.
-*  
+*
 *  rfm12b-linux is free software: you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
 *  the Free Software Foundation, either version 2 of the License, or
 *  (at your option) any later version.
-*  
+*
 *  rfm12b-linux is distributed in the hope that it will be useful,
 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *  GNU General Public License for more details.
-*  
+*
 *  You should have received a copy of the GNU General Public License
 *  along with rfm12b-linux.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -23,19 +23,17 @@
 /*
   The plaform you are building for. This is important, change it to
   whatever board you are using. You can further edit hardware-specific
-  configuration in the following files.
-  
+  configuration in the following files. 
   IMPORTANT: If you don't set this to the board you actually want to
              build for, you'll get a segfault or oops when you try to
              insert the module!
-  
+
   BOARD             NUMBER          HARDWARE-SPECIFICS
-  
   Raspberry Pi      1               platform/plat_raspberrypi.h
   Beaglebone        2               platform/plat_beaglebone.h
   Beaglebone Black  3               platform/plat_beaglebone.h
 */
-#define RFM12B_BOARD        0
+#define RFM12B_BOARD        1
 
 /*
   The name of the driver within the kernel (e.g. shows up in logs, etc...)
@@ -53,7 +51,7 @@
   The default group ID to use for each RFM12B board. You can change the
   group ID for a particular board via ioctl() when you open the device.
   You can also pass group_id=X when loading the module.
-  
+
   Each RFM12B needs to have the same group ID to talk to each other.
   Change this to whatever you want, (needs to be between 0 and 255).
 */
@@ -63,10 +61,10 @@
   The default frequency band to use for each RFM12B board. You can change
   the band for a particular board via ioctl() when you open the device.
   You can also pass band_id=X when loading the module.
-  
+
   You should only use the band for which your board was built. This should
   be written at the bottom of the RFM12B board.
-  
+
   The settings are as follows
      1 ... 433mhz
      2 ... 868mhz
@@ -77,10 +75,10 @@
   The default bit rate to use for each RFM12B board. You can change the
   bit rate for a particular board via ioctl() when you open the device.
   You can also pass bit_rate=X when loading the module.
-  
+
   The bit rate determines how fast the board sends and receives data.
   All boards need to be configured to the same rate to communicate.
-  
+
   Please refer to the datasheet for how to calculate this byte. The
   default setting is ~ 49.2kpbs, which is also what jeelib uses.
   <https://github.com/jcw/jeelib>
@@ -91,9 +89,9 @@
   Whether Jee-compatible mode is enabled by default or not. You can
   still enable Jee-compatible mode for a board either via ioctl() or
   by passing a parameter to the module when loading it.
-  
+
   Jee-compatible mode differs from normal operation by these traits
-  
+
   1) The first two bytes of a packet (header and length) are passed
      to user-space via read(), so you can inspect them.
   2) When you pass data to send via write(), the first two bytes of
@@ -110,7 +108,7 @@
   request them when in Jee-compatible mode. You can change this per
   board via ioctl() or change the default by passing a module parameter
   on load.
-  
+
   If Jee-compatible mode is enabled (see RFM12B_DEFAULT_JEE_ID), the
   driver can automatically send ACKs for packets that request them, so
   you don't have to worry about this in your user-space program.
@@ -143,15 +141,15 @@
   underflow) field in the status word. Those should be set if there was
   an overflow of data while receiving (e.g. you're not reading data fast
   enough) or if you don't supply bits to be sent fast enough, respectively.
-  
+
   However, I'm not sure if I understand those fields correctly, as disregarding
   them seems to improve reliability a lot – I need to investigate this further.
   Thus, you can enable/disable the behavior to regard a send/recv as failed
   when these bits are set in the status register.
-  
+
   Note that even if the "drop packet" behavior is disabled, you are still
   protected against corrupted data by the internal CRC16 checksum.
-  
+
   Tip: Leave these at defaults, unless you're actively investigating this.
 */
 #define RFM12B_DROP_PACKET_ON_FFOV       1
