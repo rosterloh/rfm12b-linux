d47714c0b216a2ddb6edeef37bc731183c2a1180
Andrew Bird <ajb@spheresystems.co.uk>
2014-10-23 19:30:44 +0100

    Fix header file conditional

--- examples/common/common.h
+++ examples/common/common.h
@@ -17,7 +17,7 @@
 *  along with rfm12b-linux.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#if defined(__COMMON_H__)
+#ifndef __COMMON_H__
 #define __COMMON_H__
 
 // get rfm12b stats via ioctl and print to stdout
