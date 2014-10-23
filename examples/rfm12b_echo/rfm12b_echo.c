49481234e78437c3df6797bf17b4e82d168a1519
Andrew Bird <ajb@spheresystems.co.uk>
2014-10-23 19:19:47 +0100

    Clean up trailing whitespace in examples sources

--- examples/rfm12b_echo/rfm12b_echo.c
+++ examples/rfm12b_echo/rfm12b_echo.c
@@ -1,31 +1,31 @@
 /* rfm12b-linux: linux kernel driver for the rfm12(b) RF module by HopeRF
 *  Copyright (C) 2013 Georg Kaindl
-*  
+*
 *  This file is part of rfm12b-linux.
-*  
+*
 *  rfm12b-linux is free software: you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
 *  the Free Software Foundation, either version 2 of the License, or
 *  (at your option) any later version.
-*  
+*
 *  rfm12b-linux is distributed in the hope that it will be useful,
 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *  GNU General Public License for more details.
-*  
+*
 *  You should have received a copy of the GNU General Public License
 *  along with rfm12b-linux.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /*
    DOCUMENTATION:
-   
+
    This example acts as an echo beacon: Every received packet will
    immediately be sent out again.
-   
+
    It is pretty much a combination of the read and write examples,
    so you might also want to check out their respective documentations.
-   
+
    You can use this test in conjunction with the rfm12b_chat.c example
    or with the rfm12b_echo_client Arduino example (in this case, it is
    also very useful to check the range and reliability of your setup).
@@ -58,9 +58,9 @@ int main(int argc, char** argv)
    char* devname, buf[128];
    unsigned long pkt_cnt;
    time_t tt;
-   
+
    devname = RF12_TESTS_DEV;
-   
+
    fd = open(RF12_TESTS_DEV, O_RDWR);
    if (fd < 0) {
       printf("\nfailed to open %s: %s.\n\n", devname, strerror(errno));
@@ -74,40 +74,40 @@ int main(int argc, char** argv)
    fflush(stdout);
    signal(SIGINT, sig_handler);
    signal(SIGTERM, sig_handler);
-   
+
    pkt_cnt = 0;
    running = 1;
-   do {      
+   do {
       len = read(fd, buf, RF12_BUF_LEN);
-      
+
       time (&tt);
-      
+
       if (len > 0) {
          printf("%s", ctime(&tt));
          printf("\t%i bytes read\n\t\t", len);
-         
+
          for (i=0; i<len; i++) {
             printf("%d ", buf[i]);
          }
          printf("\n");
-         
-         fflush(stdout);   
-   
+
+         fflush(stdout);
+
          pkt_cnt++;
-                  
+
          len = write(fd, buf, len);
-         
+
          printf("\techoed the packet with result: %d.\n", len);
       } else if (len < 0) {
          break;
-      }      
+      }
    } while (running);
-   
+
    print_stats(fd);
-   
+
    close(fd);
-   
+
    printf("\n\n%lu packet(s) echoed.\n\n", pkt_cnt);
-   
+
    return len;
 }
